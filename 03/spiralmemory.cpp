e6f8b14d043b4478df33934c420635dbcd32b08d
chrisbduck <>
2018-09-28 22:06:35 -0700

    Skeleton file for AOC2017 day 3

---
+++ 03/spiralmemory.cpp
@@ -0,0 +1,20 @@
+//
+// Day 3: Spiral Memory
+// https://adventofcode.com/2017/day/3
+//
+
+#include <iostream>
+
+using namespace std;
+
+// -----------------------------------------------------------------------------
+
+int main(int argc, char* argv[]) {
+	// Just a starting skeleton file for now
+	if (argc == 1) {
+		cout << "No arguments" << endl;
+		return 1;
+	}
+	cout << "Arguments" << endl;
+	return 0;
+}
